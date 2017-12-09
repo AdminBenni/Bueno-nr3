20f7916a48cf829f5486f498468462dc336969d2
AdminBenni <benediktaronsigurorsson@gmail.com>
2017-12-09 19:40:54 +0000

    Added base website layout and passwords

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 # import os
 # import sys
-# import random
 # import re
 # import json
-# from time import time as epoch
+from time import time as epoch
+from passwords import encrypt, decrypt
 from bottle import *
 # from pymysql import *
 
 @route("/res/<filename>")
-def static_skrar(filename):
-    return static_file(filename, root="./res")
+def static_skrar(filename): return static_file(filename, root="./res")
+
 @route("/res/fonts/<filename>")
-def static_skrar(filename):
-    return static_file(filename, root="./res/fonts")
+def static_skrar(filename): return static_file(filename, root="./res/fonts")
+
 @route("/res/imgs/<filename>")
-def static_skrar(filename):
-    return static_file(filename, root="./res/imgs")
+def static_skrar(filename): return static_file(filename, root="./res/imgs")
+
 @route("/res/style/<filename>")
-def static_skrar(filename):
-    return static_file(filename, root="./res/style")
+def static_skrar(filename): return static_file(filename, root="./res/style")
 
 @route("/")
 def home():
     return template("index")
 
-run()
+# run()
--- views/index.tpl
+++ views/index.tpl
@@ -5,6 +5,23 @@
         <link rel="stylesheet" href="res/style/index.css" type="text/css">
     </head>
     <body>
-        <div><p>Lol</p></div>
+        <div class="site">
+            <div class="nav">
+                <div class="logo">
+                    <img style="max-height: 100%;" src="res/imgs/eggs.svg">
+                    <div class="name-wrapper">
+                        <h1>Eggworld</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="content-wrapper">
+                <div class="main-content">
+
+                </div>
+                <div class="feed">
+
+                </div>
+            </div>
+        </div>
     </body>
 </html>
